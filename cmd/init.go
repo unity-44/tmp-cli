143324d6c240e017cd831d967c6ae5b810575990
Gregor <gregor.goddemeier@usercentrics.com>
2023-11-20 20:51:02 +0100

    create gitignore

--- cmd/init.go
+++ cmd/init.go
@@ -16,13 +16,9 @@ import (
 // will be set eighter in config or in shell config
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "initialize standard tmp-cli folder",
+	Long: `initialises standard tmp-cli folder and sets it in config or bash config. This folder will be
+	used to store all files if not set by flag`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called")
 	},
